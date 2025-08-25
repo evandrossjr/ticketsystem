62e6dc343902078bf94c67065a986674b8970d1d
Evandro Sacramento II <evandro-dev@outlook.com>
2025-08-25 16:31:43 -0300

    update UserControllerTest

--- src/test/java/com/essj/ticketsystem/controller/api/UserControllerTest.java
+++ src/test/java/com/essj/ticketsystem/controller/api/UserControllerTest.java
@@ -1,14 +1,12 @@
-package com.essj.ticketsystem;
+package com.essj.ticketsystem.controller.api;
 
 
-import com.essj.ticketsystem.controller.api.UserController;
 import com.essj.ticketsystem.dtos.UserDTO;
 import com.essj.ticketsystem.models.enums.UserRole;
 import com.essj.ticketsystem.services.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
