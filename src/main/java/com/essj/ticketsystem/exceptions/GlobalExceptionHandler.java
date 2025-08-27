283d25c7a2f4e5ba51f1e82d9444cec0cfdb67be
Evandro Sacramento II <evandro-dev@outlook.com>
2025-08-26 23:18:04 -0300

    Adding GlobalExceptionHandler IV

--- src/main/java/com/essj/ticketsystem/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/essj/ticketsystem/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,13 @@ package com.essj.ticketsystem.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler   {
@@ -19,6 +24,16 @@ public class GlobalExceptionHandler   {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
 
 
 }
