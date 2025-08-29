f71830cf24ffb8b05af8736a80f0bec3b91c7b17
Evandro Sacramento II <evandro-dev@outlook.com>
2025-08-29 14:42:32 -0300

    Diagram IaC XX-14 - created realease.yml, created db4, test with PR for diagram

--- docs/diagram.py
+++ docs/diagram.py
@@ -63,6 +63,12 @@ with Diagram(
             "Armazena dados de tickets e usuários."
         )
 
+        db5 = Database(
+            "Banco de Dados Oracle",
+            "Banco de dados em memória",
+            "Armazena dados de tickets e usuários."
+        )
+
 
     # Relações entre os componentes
     usuario >> Relationship("Usa a API REST") >> app
